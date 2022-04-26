9a79469cbb7030c3ae7f8e2ddafbab57746796c3
Toon Geens <toon.geens@xenit.eu>
2022-04-26 10:51:08 +0200

    Cleanup imports

--- src/main/java/eu/xenit/alfred/content/gateway/cors/CorsResolverProperties.java
+++ src/main/java/eu/xenit/alfred/content/gateway/cors/CorsResolverProperties.java
@@ -1,12 +1,10 @@
 package eu.xenit.alfred.content.gateway.cors;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.web.cors.CorsConfiguration;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.cors.CorsConfiguration;
 
 @ConfigurationProperties("contentcloud.gateway.cors")
 public class CorsResolverProperties {
