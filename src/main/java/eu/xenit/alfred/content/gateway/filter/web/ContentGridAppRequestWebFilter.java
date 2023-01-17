0384af27828bd2fccd72e00d669a4c9ffdca2768
Toon Geens <toon.geens@xenit.eu>
2023-01-17 11:00:13 +0100

    PR feedback

--- src/main/java/eu/xenit/alfred/content/gateway/filter/web/ContentGridAppRequestWebFilter.java
+++ src/main/java/eu/xenit/alfred/content/gateway/filter/web/ContentGridAppRequestWebFilter.java
@@ -3,6 +3,7 @@ package eu.xenit.alfred.content.gateway.filter.web;
 import eu.xenit.alfred.content.gateway.routing.ServiceTracker;
 import eu.xenit.alfred.content.gateway.servicediscovery.ContentGridApplicationMetadata;
 import eu.xenit.alfred.content.gateway.servicediscovery.ContentGridDeploymentMetadata;
+import java.util.Comparator;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.Set;
@@ -23,18 +24,17 @@ import reactor.core.publisher.Mono;
 public class ContentGridAppRequestWebFilter implements WebFilter {
 
     /**
-     * This {@link WebFilter} needs to be registered before the Spring Security WebFilter, so that authentication
-     * and authorization infrastructure can use request attributes set by this WebFilter. The Spring Security
-     * WebFilter is registered by {@code WebFluxSecurityConfiguration}.
+     * This {@link WebFilter} needs to be registered before the Spring Security WebFilter, so that authentication and
+     * authorization infrastructure can use request attributes set by this WebFilter. The Spring Security WebFilter is
+     * registered by {@code WebFluxSecurityConfiguration}.
      *
      * @see <a href="https://github.com/spring-projects/spring-boot/issues/33551">Spring Boot issue 33551</a>
-     *
      */
     public static final int CONTENTGRID_WEB_FILTER_CHAIN_FILTER_ORDER = -200;
 
-    public static final String CONTENTGRID_SERVICE_INSTANCE_ATTR = "com.contentgrid.gateway/service-instance";
-    public static final String CONTENTGRID_APP_ID_ATTR = "com.contentgrid.gateway/application-id";
-    public static final String CONTENTGRID_DEPLOY_ID_ATTR = "com.contentgrid.gateway/deployment-id";
+    public static final String CONTENTGRID_SERVICE_INSTANCE_ATTR = "com.contentgrid.gateway.service-instance";
+    public static final String CONTENTGRID_APP_ID_ATTR = "com.contentgrid.gateway.application-id";
+    public static final String CONTENTGRID_DEPLOY_ID_ATTR = "com.contentgrid.gateway.deployment-id";
 
     @NonNull
     private final ServiceTracker serviceTracker;
@@ -54,7 +54,7 @@ public class ContentGridAppRequestWebFilter implements WebFilter {
                     var appId = deploymentMetadata.getApplicationId(service);
                     var deployId = deploymentMetadata.getDeploymentId(service);
 
-                    log.info("{} {} -> app-id: {} deploy-id: {}",
+                    log.debug("{} {} -> app-id: {} deploy-id: {}",
                             exchange.getRequest().getMethod().name(), exchange.getRequest().getURI(),
                             appId.orElse(null), deployId.orElse(null));
 
@@ -69,11 +69,21 @@ public class ContentGridAppRequestWebFilter implements WebFilter {
     }
 
     private Optional<ServiceInstance> selectService(ServerWebExchange exchange, Set<ServiceInstance> candidates) {
+
         if (candidates.size() > 1) {
-            log.info("multiple matches for {}", exchange.getRequest().getURI().getHost());
-            candidates.forEach(c -> log.info("-- {}", c.getHost()));
+            // logging a warning until we have a better service selection
+            log.warn("multiple matches {}: {}", exchange.getRequest().getURI().getHost(), candidates.stream()
+                    .map(service -> deploymentMetadata.getDeploymentId(service).orElse("<none>")).toList());
         }
-        return candidates.stream().findFirst();
+
+        return candidates.stream()
+
+                // sorting based on deployment-id alphabetical order, to get at least a stable selection
+                .sorted((service1, service2) -> {
+                    var d1 = deploymentMetadata.getDeploymentId(service1).orElse("");
+                    var d2 = deploymentMetadata.getDeploymentId(service2).orElse("");
+                    return d1.compareTo(d2);
+                }).findFirst();
     }
 
     private Set<ServiceInstance> findAppServices(ServerWebExchange exchange) {
@@ -86,3 +96,4 @@ public class ContentGridAppRequestWebFilter implements WebFilter {
     }
 
 }
+
