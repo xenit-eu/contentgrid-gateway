49c42673706439831a30625e3bded506ec2674d1
Toon Geens <toon.geens@xenit.eu>
2023-01-26 10:59:36 +0100

    Runtime OAuth2 login should only match app-requests that resolve to app-id

--- src/main/java/eu/xenit/alfred/content/gateway/runtime/RuntimeRequestResolver.java
+++ src/main/java/eu/xenit/alfred/content/gateway/runtime/RuntimeRequestResolver.java
@@ -1,11 +1,21 @@
 package eu.xenit.alfred.content.gateway.runtime;
 
 import java.util.Optional;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher.MatchResult;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 public interface RuntimeRequestResolver {
 
     Optional<String> resolveApplicationId(ServerWebExchange exchange);
 
     Optional<String> resolveDeploymentId(ServerWebExchange exchange);
+
+    default ServerWebExchangeMatcher matcher() {
+        return exchange -> resolveApplicationId(exchange)
+                .map(appId -> MatchResult.match())
+                .orElse(MatchResult.notMatch());
+    }
+
 }
