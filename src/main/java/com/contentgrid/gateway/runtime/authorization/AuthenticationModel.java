f2ef819e391d8deedd81b80539321a30892f556e
Lars Vierbergen <lars.vierbergen@xenit.eu>
2024-07-03 12:19:56 +0200

    Move OPA AuthenticationModel to use AuthenticationDetails instead of parsing claims directly

--- src/main/java/com/contentgrid/gateway/runtime/authorization/AuthenticationModel.java
+++ src/main/java/com/contentgrid/gateway/runtime/authorization/AuthenticationModel.java
@@ -1,69 +1,123 @@
 package com.contentgrid.gateway.runtime.authorization;
 
+import com.contentgrid.gateway.security.authority.Actor.ActorType;
+import com.contentgrid.gateway.security.authority.AuthenticationDetails;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 import lombok.Builder;
+import lombok.RequiredArgsConstructor;
 import lombok.Value;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.ClaimAccessor;
-import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
 
 @Value
 @Builder
 public class AuthenticationModel {
-    boolean authenticated;
 
-    Map<String, Object> principal;
+    AuthenticationKind kind;
 
-    String issuer;
-    @JsonProperty("authenticated_at")
-    Instant authenticatedAt;
-    String acr;
+    PrincipalModel principal;
+
+    ActorModel actor;
+
+    public enum AuthenticationKind {
+        @JsonProperty("anonymous")
+        ANONYMOUS,
+        @JsonProperty("user")
+        USER,
+        @JsonProperty("delegated")
+        DELEGATED,
+        @JsonProperty("system")
+        SYSTEM
+    }
+
+    @JsonProperty("authenticated")
+    public boolean isAuthenticated() {
+        return kind != AuthenticationKind.ANONYMOUS;
+    }
+
+    @Value
+    public static class ActorModel {
+
+        ActorKind kind;
+        String sub;
+    }
+
+    @Value
+    public static class PrincipalModel {
+
+        ActorKind kind;
+        @JsonAnyGetter
+        Map<String, Object> claims;
+
+    }
+
+    @RequiredArgsConstructor
+    public enum ActorKind {
+        @JsonProperty("user")
+        USER(ActorType.USER),
+        @JsonProperty("extension")
+        EXTENSION(ActorType.EXTENSION);
+        private final ActorType actorType;
+
+        public static ActorKind fromType(ActorType actorType) {
+            for (ActorKind actorKind : values()) {
+                if (actorKind.actorType == actorType) {
+                    return actorKind;
+                }
+            }
+            throw new IllegalArgumentException("ActorType '%s' is not mapped to any ActorKind.".formatted(actorType));
+        }
+    }
 
     public static AuthenticationModel from(Authentication authenticationContext) {
-        var claims = extractClaims(authenticationContext);
+        var maybeAuthenticationDetails = authenticationContext.getAuthorities()
+                .stream()
+                .filter(AuthenticationDetails.class::isInstance)
+                .map(AuthenticationDetails.class::cast)
+                .findFirst();
+        if (maybeAuthenticationDetails.isEmpty()) {
+            return AuthenticationModel.builder()
+                    .kind(AuthenticationKind.ANONYMOUS)
+                    .build();
+        }
+
+        var authenticationDetails = maybeAuthenticationDetails.get();
+
         return AuthenticationModel.builder()
-                .authenticated(!(authenticationContext instanceof AnonymousAuthenticationToken))
-                .principal(createPrincipal(claims))
-                .issuer(claims.getClaimAsString("iss"))
-                .authenticatedAt(claims.getClaimAsInstant("auth_time"))
-                .acr(claims.getClaimAsString("acr"))
+                .kind(createAuthenticationKind(authenticationDetails))
+                .principal(createPrincipal(authenticationDetails))
+                .actor(createActor(authenticationDetails))
                 .build();
     }
 
-    private static Map<String, Object> createPrincipal(ClaimAccessor claims) {
-        var principal = new HashMap<String, Object>();
-
-        for (String claimName : claims.getClaims().keySet()) {
-            switch (claimName) {
-                case "preferred_username" -> principal.put("username", claims.getClaimAsString(claimName));
-                case "email", "sub" -> principal.put(claimName, claims.getClaimAsString(claimName));
-            }
-            if(claimName.startsWith("contentgrid:")) {
-                principal.put(claimName, claims.getClaim(claimName));
-            }
+    private static AuthenticationKind createAuthenticationKind(AuthenticationDetails authenticationDetails) {
+        if (authenticationDetails.getActor() != null) {
+            return AuthenticationKind.DELEGATED;
         }
-
-        return principal;
+        return switch (authenticationDetails.getPrincipal().getType()) {
+            case USER -> AuthenticationKind.USER;
+            case EXTENSION -> AuthenticationKind.SYSTEM;
+        };
     }
 
-    private static ClaimAccessor extractClaims(Authentication authenticationContext) {
-        var principal = authenticationContext.getPrincipal();
-
-        if(principal instanceof ClaimAccessor claimAccessor) {
-            return claimAccessor;
-        }
-
-        // fallback to check authorities on the auth-object
-        var claims =  authenticationContext.getAuthorities().stream()
-                .filter(OAuth2UserAuthority.class::isInstance)
-                .map(OAuth2UserAuthority.class::cast)
-                .findAny()
-                .map(OAuth2UserAuthority::getAttributes)
-                .orElse(Map.of());
-        return () -> claims;
+    private static PrincipalModel createPrincipal(AuthenticationDetails authenticationDetails) {
+        return new PrincipalModel(
+                ActorKind.fromType(authenticationDetails.getPrincipal().getType()),
+                authenticationDetails.getPrincipal().getClaims().getClaims()
+        );
     }
+
+    private static ActorModel createActor(AuthenticationDetails authenticationDetails) {
+        var actor = authenticationDetails.getActor();
+        if (actor == null) {
+            return null;
+        }
+        return new ActorModel(
+                ActorKind.fromType(actor.getType()),
+                actor.getClaims().getClaimAsString(JwtClaimNames.SUB)
+        );
+    }
+
 }
--- src/test/java/com/contentgrid/gateway/runtime/authorization/AuthenticationModelTest.java
+++ src/test/java/com/contentgrid/gateway/runtime/authorization/AuthenticationModelTest.java
@@ -2,90 +2,125 @@ package com.contentgrid.gateway.runtime.authorization;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.contentgrid.gateway.runtime.authorization.AuthenticationModel.ActorKind;
+import com.contentgrid.gateway.runtime.authorization.AuthenticationModel.AuthenticationKind;
+import com.contentgrid.gateway.runtime.security.authority.ClaimUtil;
+import com.contentgrid.gateway.security.authority.UserGrantedAuthorityConverter;
+import com.contentgrid.gateway.security.authority.Actor;
+import com.contentgrid.gateway.security.authority.Actor.ActorType;
+import com.contentgrid.gateway.security.authority.ActorConverter;
+import com.contentgrid.gateway.security.authority.DelegatedAuthenticationDetailsGrantedAuthority;
+import com.contentgrid.gateway.security.authority.PrincipalAuthenticationDetailsGrantedAuthority;
 import java.net.URI;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.TestingAuthenticationToken;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.core.ClaimAccessor;
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.jwt.JoseHeaderNames;
+import org.springframework.security.oauth2.jwt.Jwt;
 
 class AuthenticationModelTest {
+
+    private static final String USER_ISSUER = "https://authentication.invalid/realms/my-user-realm";
+    private static final String EXTENSION_SYSTEM_ISSUER = "https://extensions.invalid/authentication/system";
+    private static final Converter<ClaimAccessor, Actor> ACTOR_CONVERTER = new ActorConverter(iss -> true,
+            ActorType.USER, ClaimUtil::userClaims);
 
     @Test
     void fromOidcUser() {
         var instant = Instant.now();
 
         var idToken = OidcIdToken.withTokenValue("dummy")
+                .issuer(USER_ISSUER)
                 .subject("04c2cbec-faad-4dc8-ba6f-edb3d5b902e9")
-                .claim("preferred_username", "alice")
+                .claim(StandardClaimNames.EMAIL, "alice@wonderland.example")
+                .claim(StandardClaimNames.PREFERRED_USERNAME, "alice")
+                .claim("contentgrid:custom", List.of("blue", "green"))
                 .issuedAt(instant)
                 .expiresAt(instant.plus(5, ChronoUnit.MINUTES))
                 .authTime(instant)
                 // This is actually how we receive the issuer, as an URI
-                .claim(IdTokenClaimNames.ISS, URI.create("https://auth.contentgrid.com/auth/realms/my-org"))
+                .claim(IdTokenClaimNames.ISS, URI.create(USER_ISSUER))
                 .authorizedParty("contentgrid-gateway")
                 .build();
-        var userInfo = new OidcUserInfo(Map.of(
-                "contentgrid:custom", List.of("blue", "green"),
-                "email_verified", false
-        ));
+        var userInfo = new OidcUserInfo(idToken.getClaims());
         var userRole = new OidcUserAuthority(idToken, userInfo);
         var oidcUser = new DefaultOidcUser(Set.of(userRole), idToken, userInfo, "preferred_username");
 
-        var auth = new TestingAuthenticationToken(oidcUser, null, AuthorityUtils.NO_AUTHORITIES);
+        var converter = new UserGrantedAuthorityConverter(ACTOR_CONVERTER);
+
+        var auth = new TestingAuthenticationToken(oidcUser, null,
+                new ArrayList<>(converter.mapAuthorities(List.of(userRole))));
 
         var model = AuthenticationModel.from(auth);
 
-        assertThat(model.isAuthenticated()).isEqualTo(true);
+        assertThat(model.getKind()).isEqualTo(AuthenticationKind.USER);
 
-        assertThat(model.getPrincipal()).containsExactlyInAnyOrderEntriesOf(Map.of(
-                "username", "alice",
+        assertThat(model.getPrincipal().getKind()).isEqualTo(ActorKind.USER);
+        assertThat(model.getPrincipal().getClaims()).containsExactlyInAnyOrderEntriesOf(Map.of(
+                "iss", URI.create(USER_ISSUER),
                 "sub", idToken.getSubject(),
+                "email", "alice@wonderland.example",
                 "contentgrid:custom", List.of("blue", "green")
         ));
 
-        assertThat(model.getAuthenticatedAt()).isEqualTo(instant);
-        assertThat(model.getIssuer()).isEqualTo("https://auth.contentgrid.com/auth/realms/my-org");
+        assertThat(model.getActor()).isNull();
     }
 
     @Test
     void fromJwtAccessToken() {
         var instant = Instant.now();
 
-        var jwtToken = new ClaimAccessor() {
-
-            @Override
-            public Map<String, Object> getClaims() {
-                return Map.of("sub", "04c2cbec-faad-4dc8-ba6f-edb3d5b902e9",
+        var jwtToken = new Jwt(
+                "X",
+                Instant.now(),
+                Instant.MAX,
+                Map.of(
+                        JoseHeaderNames.TYP, "JWT",
+                        JoseHeaderNames.ALG, "none"
+                ),
+                Map.of(
+                        "iss", USER_ISSUER,
+                        "sub", "04c2cbec-faad-4dc8-ba6f-edb3d5b902e9",
                         "iat", instant,
                         "preferred_username", "alice",
+                        "name", "Alice",
+                        "email", "alice@wonderland.example",
                         "contentgrid:custom", List.of("blue", "green"),
                         "email_verified", false
-                );
+                )
+        );
 
-            }
-        };
-        var auth = new TestingAuthenticationToken(jwtToken, null, AuthorityUtils.NO_AUTHORITIES);
+        var converter = new UserGrantedAuthorityConverter(ACTOR_CONVERTER);
+
+        var auth = new TestingAuthenticationToken(jwtToken, null, new ArrayList<>(converter.convert(jwtToken)));
 
         var model = AuthenticationModel.from(auth);
 
         assertThat(model.isAuthenticated()).isEqualTo(true);
-        assertThat(model.getPrincipal()).containsAllEntriesOf(Map.of(
-                "username", "alice",
+        assertThat(model.getPrincipal().getKind()).isEqualTo(ActorKind.USER);
+        assertThat(model.getPrincipal().getClaims()).containsAllEntriesOf(Map.of(
+                "iss", USER_ISSUER,
+                "email", "alice@wonderland.example",
                 "sub", "04c2cbec-faad-4dc8-ba6f-edb3d5b902e9",
                 "contentgrid:custom", List.of("blue", "green")
         ));
+
+        assertThat(model.getActor()).isNull();
     }
 
     @Test
@@ -94,9 +129,71 @@ class AuthenticationModelTest {
         var model = AuthenticationModel.from(anonymousToken);
 
         assertThat(model.isAuthenticated()).isFalse();
-        assertThat(model.getAuthenticatedAt()).isNull();
-        assertThat(model.getIssuer()).isNull();
-        assertThat(model.getAcr()).isNull();
-        assertThat(model.getPrincipal()).isEmpty();
+        assertThat(model.getKind()).isEqualTo(AuthenticationKind.ANONYMOUS);
+        assertThat(model.getPrincipal()).isNull();
+        assertThat(model.getActor()).isNull();
+    }
+
+    @Test
+    void fromServiceAccountAccessToken() {
+        var model = AuthenticationModel.from(new TestingAuthenticationToken(null, null, List.of(
+                new PrincipalAuthenticationDetailsGrantedAuthority(new Actor(
+                        ActorType.EXTENSION,
+                        () -> Map.of(
+                                "iss", EXTENSION_SYSTEM_ISSUER,
+                                "sub", "extension123"
+                        ),
+                        null
+                ))
+        )));
+
+        assertThat(model.isAuthenticated()).isTrue();
+        assertThat(model.getKind()).isEqualTo(AuthenticationKind.SYSTEM);
+        assertThat(model.getPrincipal().getKind()).isEqualTo(ActorKind.EXTENSION);
+        assertThat(model.getPrincipal().getClaims()).containsExactlyInAnyOrderEntriesOf(Map.of(
+                "iss", EXTENSION_SYSTEM_ISSUER,
+                "sub", "extension123"
+        ));
+
+        assertThat(model.getActor()).isNull();
+    }
+
+    @Test
+    void fromDelegatedAccountAccessToken() {
+        var model = AuthenticationModel.from(new TestingAuthenticationToken(null, null, List.of(
+                new DelegatedAuthenticationDetailsGrantedAuthority(
+                        new Actor(
+                                ActorType.USER,
+                                () -> Map.of(
+                                        "iss", USER_ISSUER,
+                                        "sub", "user",
+                                        "contentgrid:claim1", "value1"
+                                ),
+                                null
+                        ),
+                        new Actor(
+                                ActorType.EXTENSION,
+                                () -> Map.of(
+                                        "iss", EXTENSION_SYSTEM_ISSUER,
+                                        "sub", "extension1"
+                                ),
+                                null
+                        )
+                )
+        )));
+
+        assertThat(model.isAuthenticated()).isTrue();
+        assertThat(model.getKind()).isEqualTo(AuthenticationKind.DELEGATED);
+
+        assertThat(model.getPrincipal().getKind()).isEqualTo(ActorKind.USER);
+        assertThat(model.getPrincipal().getClaims()).containsExactlyInAnyOrderEntriesOf(Map.of(
+                "iss", USER_ISSUER,
+                "sub", "user",
+                "contentgrid:claim1", "value1"
+        ));
+
+        assertThat(model.getActor().getKind()).isEqualTo(ActorKind.EXTENSION);
+        assertThat(model.getActor().getSub()).isEqualTo("extension1");
+
     }
 }
