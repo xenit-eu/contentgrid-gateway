926be8d1dd63ee0ba9d1a3d4c4962cf2d4920984
niels cappelle <niels.cappelle@xenit.eu>
2025-03-05 09:00:39 +0100

    replace deprecated ExpressionJsonConverter

--- src/main/java/com/contentgrid/gateway/runtime/security/jwt/issuer/RuntimeJwtClaimsResolver.java
+++ src/main/java/com/contentgrid/gateway/runtime/security/jwt/issuer/RuntimeJwtClaimsResolver.java
@@ -5,11 +5,13 @@ import com.contentgrid.gateway.runtime.application.DeploymentId;
 import com.contentgrid.gateway.runtime.web.ContentGridAppRequestWebFilter;
 import com.contentgrid.gateway.security.authority.AuthenticationDetails;
 import com.contentgrid.gateway.security.jwt.issuer.JwtClaimsResolver;
-import com.contentgrid.thunx.encoding.json.ExpressionJsonConverter;
+import com.contentgrid.thunx.encoding.ThunkExpressionEncoder;
+import com.contentgrid.thunx.encoding.json.JsonThunkExpressionCoder;
 import com.contentgrid.thunx.predicates.model.ThunkExpression;
 import com.contentgrid.thunx.spring.security.ReactivePolicyAuthorizationManager;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.JWTClaimsSet.Builder;
+import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
 import org.springframework.web.server.ServerWebExchange;
@@ -17,7 +19,7 @@ import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 public class RuntimeJwtClaimsResolver implements JwtClaimsResolver {
-    private static final ExpressionJsonConverter thunxExpressionConverter = new ExpressionJsonConverter();
+    private static final ThunkExpressionEncoder thunxExpressionEncoder = new JsonThunkExpressionCoder();
 
     @Override
     public Mono<JWTClaimsSet> resolveAdditionalClaims(ServerWebExchange exchange, AuthenticationDetails authenticationDetails) {
@@ -28,7 +30,7 @@ public class RuntimeJwtClaimsResolver implements JwtClaimsResolver {
         var jwtClaimsBuilder = new Builder();
 
         if(abacPolicyPredicate != null) {
-            jwtClaimsBuilder.claim("x-abac-context", thunxExpressionConverter.encode(abacPolicyPredicate));
+            jwtClaimsBuilder.claim("x-abac-context", new String(thunxExpressionEncoder.encode(abacPolicyPredicate), StandardCharsets.UTF_8));
         }
 
         return Mono.just(
